846108888f1d4a3140a71d0e728c056e133c0169
Smit Sachin Sanghvi <162765296+SMIT-1803@users.noreply.github.com>
2026-04-01 23:56:35 -0700

    Delete frontend directory

--- frontend/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../store/authSlice.js"
-export const store = configureStore({
-    reducer:{
-        auth:authReducer}
-});
